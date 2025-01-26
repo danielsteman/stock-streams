195c463a8149b081d81cb5794264ee2c69c6a5ae
Daniel Steman <daniel.steman@bridgefund.nl>
2025-01-26 17:09:14 +0100

    add small example where i listen for new binance price points

--- stockstreams/main.py
+++ stockstreams/main.py
@@ -1,3 +1,30 @@
-import yfinance as yf
+import asyncio
+from websockets.asyncio.client import connect
+from websockets import exceptions
+import json
 
-dat = yf.Ticker("MSFT")
+
+BASE_URL = "wss://fstream.binance.com"
+ENDPOINT = "ws/bnbusdt@aggTrade"
+
+
+async def hello():
+    url = f"{BASE_URL}/{ENDPOINT}"
+    async with connect(url) as websocket:
+        print("Connected to Binance WebSocket!")
+
+        try:
+            while True:
+                message = await websocket.recv()  # Receive messages from the WebSocket
+                data = json.loads(message)
+                # Extract relevant data from the message
+                price = data["p"]  # 'p' is the price field for aggTrade
+                quantity = data["q"]  # 'q' is the quantity field for aggTrade
+                print(f"Price: {price}, Quantity: {quantity}")
+
+        except exceptions.ConnectionClosed as e:
+            print(f"Connection closed: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(hello())
